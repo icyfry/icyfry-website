cd2ac6176594d3541623a08a45dd6be003d7a837
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2019-10-07 00:24:56 +0200

    setup simple vue.js project

---
+++ src/components/Links.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="links">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li><a href="http://github.icyfry.io" target="_blank" rel="noopener">github</a></li>
+      <li><a href="http://gitlab.icyfry.io" target="_blank" rel="noopener">gitlab</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Links extends Vue {
+  @Prop() private title!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ tests/unit/links.spec.ts
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
+import Links from '@/components/Links.vue';
+
+describe('Links.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'new title';
+    const wrapper = shallowMount(Links, {
+      propsData: { title },
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
+});
