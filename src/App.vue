c4f628ec19748812a4c4d0b922666684f8fa3d31
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2019-10-08 21:16:38 +0200

    add bottom and hide header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
+    <!--
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    -->
     <router-view/>
+    <bottom :url="git"/>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import {Route} from 'vue-router';
+import Bottom from '@/components/Bottom.vue';
+
+@Component({
+  components: {
+    Bottom,
+  },
+})
+export default class App extends Vue {
+
+  @Prop({default: 'https://github.com/icyfry/website'}) private git: any;
+
+}
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Press Start 2P', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
 }
@@ -28,4 +46,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
---
+++ src/components/Bottom.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bottom">
+      icyfry.io - 2019 ({{url}})
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Bottom extends Vue {
+  @Prop() private url!: string;
+}
+</script>
+
+<style scoped>
+
+.bottom {
+  margin-top: 3px;
+  font-size: 5pt;
+}
+
+</style>
