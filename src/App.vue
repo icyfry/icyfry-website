6b4431ee34de9df47095a85eae91183363d8a33c
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2020-07-03 02:36:29 +0200

    add parameters for configcat

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ import Links from '@/components/Links.vue';
 import * as configcat from 'configcat-js';
 import { IConfigCatClient } from 'configcat-common/lib/ConfigCatClient';
 import { LogLevel } from 'configcat-common';
+import { User } from 'configcat-common/lib/RolloutEvaluator';
 
 
 @Component({
@@ -54,7 +55,11 @@ export default class App extends Vue {
   }
 
   private readShowGitInformation() {
-    this.configCatClient.getValueAsync('show_git_infos',  false)
+
+    const user: User = new User((this.$route.query.id) as string);
+    user.custom = { group: (this.$route.query.group) as string};
+
+    this.configCatClient.getValueAsync('show_git_infos', false, user)
       .then( (value) => {
         this.showGitInformation = value;
     });
